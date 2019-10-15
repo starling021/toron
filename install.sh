d852fe180629e5153ce64c83102f720572016771
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-15 18:19:39 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -22,39 +22,24 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-if [[ -d /System/Library/CoreServices/Finder.app ]]
-then
-RSA="\033[1;31m"
-YSA="\033[1;33m"
-#blue start 
-	BS="-e \033[34m"
-#color end
-	CE="\033[0m"
-#red start
-	RS="-e \033[31m"
-#green start
-	GNS="-e \033[32m"
-else
-RSA="\e[1;31m"
-YSA="\e[1;33m"
-#blue start 
-	BS="-e \e[0;34m"
-#color end
-	CE="\e[0m"
-#red start
-	RS="-e \e[0;31m"
-#green start
-	GNS="-e \e[0;32m"
-fi
+N="\033[1;37m"
+C="\033[0m"
+
+CE="\033[0m"
+RS="\033[1;31m"
+YS="\033[1;33m"
+BS="-e \033[34m"
+
+R="\033[31m"
 
 WHO="$( whoami )"
 
 if [[ "$WHO" != "root" ]]
 then
 sleep 1
-echo -e "$RSA"run it as"$CE" "$YSA"root"$CE"
+echo -e "$RS"run it as"$CE" "$YS"root"$CE"
 sleep 1
-echo -e "$RSA"or use"$CE" "$YSA"sudo"$CE"
+echo -e "$RS"or use"$CE" "$YS"sudo"$CE"
 sleep 1
 exit
 fi
@@ -63,37 +48,14 @@ if [[ -d ~/copycat ]]
 then
 cd ~/copycat/bin
 {
-cp copycat /usr/local/bin
-chmod +x /usr/local/bin/copycat
-cp copycat-radar /usr/local/bin
-chmod +x /usr/local/bin/copycat-radar
 cp copycat /bin
-chmod +x /bin/copycat
-cp copycat-radar /bin
-chmod +x /bin/copycat-radar
-chmod +x ~/copycat/copycat
-chmod +x ~/copycat/copycat-radar
-} &> /dev/null
-else
-cd ~
-{
-git clone https://github.com/entynetproject/copycat.git
-cd ~/copycat/bin
 cp copycat /usr/local/bin
-chmod +x /usr/local/bin/copycat
-cp copycat-radar /usr/local/bin
-chmod +x /usr/local/bin/copycat-radar
-cp copycat /bin
 chmod +x /bin/copycat
-cp copycat-radar /bin
-chmod +x /bin/copycat-radar
-chmod +x ~/copycat/copycat
-chmod +x ~/copycat/copycat-radar
-} &> /dev/null
-fi
-sleep 0.5
-echo  
+chmod +x /usr/local/bin/copycat
 cd ~/copycat
+} &> /dev/null
+sleep 0.5
+echo
 cat banner/banner.txt
 echo
 
@@ -111,10 +73,8 @@ echo ""$BS"Installing dependencies..."$CE""
 else 
 echo ""$BS"Installing dependencies..."$CE""
 pkg update
-pkg install python
-pkg install python-pip
-pkg install python2
-pkg install python2-pip
+pkg install python3
+pkg install python3-pip
 fi
 fi
 
@@ -126,10 +86,8 @@ echo ""$BS"Installing dependencies..."$CE""
 else
 echo ""$BS"Installing dependencies..."$CE""
 apt-get update
-apt-get install python
-apt-get install python-pip
-apt-get install python2
-apt-get install python2-pip
+apt-get install python3
+apt-get install python3-pip
 fi
 fi
 
@@ -141,14 +99,13 @@ echo ""$BS"Installing dependencies..."$CE""
 else
 echo ""$BS"Installing dependencies..."$CE""
 apt-get update
-apt-get install python
-apt-get install python-pip
-apt-get install python2
-apt-get install python2-pip
+apt-get install python3
+apt-get install python3-pip
 fi
 fi
 
 else
+
 read -p $'Select your architecture (amd/intel/arm) ' CONF
 if [[ "$CONF" = "" ]]
 then
@@ -166,10 +123,8 @@ echo ""$BS"Installing dependencies..."$CE""
 else 
 echo ""$BS"Installing dependencies..."$CE""
 pkg update
-pkg install python
-pkg install python-pip
-pkg install python2
-pkg install python2-pip
+pkg install python3
+pkg install python3-pip
 fi
 fi
 
@@ -181,10 +136,8 @@ echo ""$BS"Installing dependencies..."$CE""
 else
 echo ""$BS"Installing dependencies..."$CE""
 apt-get update
-apt-get install python
-apt-get install python-pip
-apt-get install python2
-apt-get install python2-pip
+apt-get install python3
+apt-get install python3-pip
 fi
 fi
 
@@ -196,47 +149,132 @@ echo ""$BS"Installing dependencies..."$CE""
 else
 echo ""$BS"Installing dependencies..."$CE""
 apt-get update
-apt-get install python
-apt-get install python-pip
-apt-get install python2
-apt-get install python2-pip
+apt-get install python3
+apt-get install python3-pip
 fi
 fi
 fi
 
-if [[ -f /usr/local/bin/shodan ]]
-then
-sleep 0.5
-     if [[ -f ~/.shodan/api_key ]]
-     then
-     sleep 0.5
-     else
-     sleep 0.5
-     SHO="PSKINdQe1GyxGgecYz2191H2JoS9qvgD"
-     {
-     shodan init "$SHO"
-     } &> /dev/null
-     fi
+{
+pip3 install setuptools
+pip3 install -r related.txt
+} &> /dev/null
+
 else
-     sleep 0.5
-     {
-     pip uninstall shodan -y
-     pip install shodan
-     } &> /dev/null
-sleep 0.5
-SHO="PSKINdQe1GyxGgecYz2191H2JoS9qvgD"
+cd ~
 {
-shodan init "$SHO"
+git clone https://github.com/entynetproject/copycat.git
+cd ~/copycat/bin
+cp copycat /bin
+cp copycat /usr/local/bin
+chmod +x /bin/copycat
+chmod +x /usr/local/bin/copycat
+cd ~/copycat
 } &> /dev/null
+sleep 0.5
+echo
+cat banner/banner.txt
+echo
+
+if [[ -f /etc/copycat.conf ]]
+then
+
+CONF="$( cat /etc/copycat.conf )"
+sleep 1
+
+if [[ "$CONF" = "arm" ]]
+then
+if [[ -d /System/Library/CoreServices/SpringBoard.app ]]
+then
+echo ""$BS"Installing dependencies..."$CE""
+else 
+echo ""$BS"Installing dependencies..."$CE""
+pkg update
+pkg install python3
+pkg install python3-pip
+fi
+fi
+
+if [[ "$CONF" = "amd" ]]
+then
+if [[ -d /System/Library/CoreServices/Finder.app ]]
+then
+echo ""$BS"Installing dependencies..."$CE""
+else
+echo ""$BS"Installing dependencies..."$CE""
+apt-get update
+apt-get install python3
+apt-get install python3-pip
+fi
+fi
+
+if [[ "$CONF" = "intel" ]]
+then
+if [[ -d /System/Library/CoreServices/Finder.app ]]
+then
+echo ""$BS"Installing dependencies..."$CE""
+else
+echo ""$BS"Installing dependencies..."$CE""
+apt-get update
+apt-get install python3
+apt-get install python3-pip
+fi
+fi
+
+else
+
+read -p $'Select your architecture (amd/intel/arm) ' CONF
+if [[ "$CONF" = "" ]]
+then
+exit
+else
+echo "$CONF" >> /etc/copycat.conf
+fi
+sleep 1
+
+if [[ "$CONF" = "arm" ]]
+then
+if [[ -d /System/Library/CoreServices/SpringBoard.app ]]
+then
+echo ""$BS"Installing dependencies..."$CE""
+else 
+echo ""$BS"Installing dependencies..."$CE""
+pkg update
+pkg install python3
+pkg install python3-pip
+fi
+fi
+
+if [[ "$CONF" = "amd" ]]
+then
+if [[ -d /System/Library/CoreServices/Finder.app ]]
+then
+echo ""$BS"Installing dependencies..."$CE""
+else
+echo ""$BS"Installing dependencies..."$CE""
+apt-get update
+apt-get install python3
+apt-get install python3-pip
+fi
+fi
+
+if [[ "$CONF" = "amd" ]]
+then
+if [[ -d /System/Library/CoreServices/Finder.app ]]
+then
+echo ""$BS"Installing dependencies..."$CE""
+else
+echo ""$BS"Installing dependencies..."$CE""
+apt-get update
+apt-get install python3
+apt-get install python3-pip
+fi
+fi
 fi
 
 {
-SED="$( cat ~/.shodan/api_key )"
+pip3 install setuptools
+pip3 install -r requirements.txt
 } &> /dev/null
-if [[ "$SED" != "PSKINdQe1GyxGgecYz2191H2JoS9qvgD" ]]
-then 
-{
-rm ~/.shodan/api_key
-echo PSKINdQe1GyxGgecYz2191H2JoS9qvgD >> ~/.shodan/api_key
-} &> /dev/null
+
 fi
