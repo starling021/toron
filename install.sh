063ebe784e72ea0ab3fc6a12598b2d09b8223599
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-11-02 14:49:47 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -123,7 +123,6 @@ else
 echo ""$BS"Installing dependencies..."$CE""
 pkg update
 pkg -y install python
-pkg -y install termux-exec
 fi
 fi
 
@@ -199,7 +198,6 @@ else
 echo ""$BS"Installing dependencies..."$CE""
 pkg update
 pkg -y install python
-pkg -y install termux-exec
 fi
 fi
 
