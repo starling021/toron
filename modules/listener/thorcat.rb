1ba1a6ae08423d7892fa2c650ea21f564c625635
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-04-28 12:47:32 +0300

    Update thorcat.rb

--- modules/listener/thorcat.rb
+++ modules/listener/thorcat.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
 #            ---------------------------------------------------
-#                             Thoron Framework                                                                                      
+#                             Thoron Framework                                
 #            ---------------------------------------------------
-#                Copyright (C) <2019-2020>  <Entynetproject>
+#                  Copyright (C) <2020>  <Entynetproject>       
 #
 #        This program is free software: you can redistribute it and/or modify
 #        it under the terms of the GNU General Public License as published by
