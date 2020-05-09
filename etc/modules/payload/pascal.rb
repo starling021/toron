ac313852cb9c73d9d24bd4538fe211cddfc48f2f
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-05-09 15:33:12 +0300

    Update pascal.rb

--- etc/modules/payload/pascal.rb
+++ etc/modules/payload/pascal.rb
@@ -30,20 +30,21 @@ require 'ostruct'
 
 options = OpenStruct.new
 OptionParser.new do |opt|
-  opt.on('-h', '--local-host <local_host>', 'Local host.') { |o| options.local_host = o }
-  opt.on('-p', '--local-port <local_port>', 'Local port.') { |o| options.local_port = o }
-  opt.on('-s', '--target-shell <target_shell>', 'Target shell.') { |o| options.target_shell = o }
+    opt.on('-l', '--local-host <local_host>', 'Local host.') { |o| options.local_host = o }
+    opt.on('-p', '--local-port <local_port>', 'Local port.') { |o| options.local_port = o }
+    opt.on('-s', '--target-shell <target_shell>', 'Target shell.') { |o| options.target_shell = o }
     opt.on('-o', '--output-path <output_path>', 'Output path.') { |o| options.output_path = o }
     opt.on('-h', '--help', "Show options.") do
-        puts "Usage: code.rb [-h] --local-host=<local_host> --local-port=<local_port>"
-        puts "               --target-shell=<target_shell> --output-path=<output_path>"
+        puts "Usage: pascal.rb [-h] --local-host=<local_host> --local-port=<local_port>"
+        puts "                 --target-shell=<target_shell> --output-path=<output_path>"
         puts ""
+        puts "  -h, --help                     Show options."
         puts "  --local-host=<local_host>      Local host."
         puts "  --local-port=<local_port>      Local port."
         puts "  --target-shell=<target_shell>  Target shell."
         puts "  --output-path=<output_path>    Output path."
+        abort()
     end
-
 end.parse!
 
 host = options.local_host
@@ -52,9 +53,10 @@ shell = options.target_shell
 file = options.output_path
 
 if not host or not port or not shell or not file
-    puts "Usage: pascal.rb --local-host=<local_host> --local-port=<local_port>"
+    puts "Usage: pascal.rb [-h] --local-host=<local_host> --local-port=<local_port>"
     puts "                 --target-shell=<target_shell> --output-path=<output_path>"
     puts ""
+    puts "  -h, --help                     Show options."
     puts "  --local-host=<local_host>      Local host."
     puts "  --local-port=<local_port>      Local port."
     puts "  --target-shell=<target_shell>  Target shell."
