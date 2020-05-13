601f75440823de464be103c06103ea4de7efc035
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-05-14 02:39:07 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -33,7 +33,7 @@ printf '\033]2;uninstall.sh\a'
 
 if [[ $EUID -ne 0 ]]
 then
-   echo -e ""$RS"[-]"$WS" This script must be run as root!"$CE"" 1>&2
+   echo -e ""$RS"[-]"$WS" This script must be run as root!"$CE""
    exit
 fi
 
