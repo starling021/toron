66d428781b0ef394c8e8c4a854a19a5b1faffcb3
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-06-01 20:01:42 +0300

    Update thorcat.rb

--- modules/listener/thorcat.rb
+++ modules/listener/thorcat.rb
@@ -32,9 +32,28 @@ options = OpenStruct.new
 OptionParser.new do |opt|
   opt.on('-h', '--local-host <local_host>', 'Local host.') { |o| options.local_host = o }
   opt.on('-p', '--local-port <local_port>', 'Local port.') { |o| options.local_port = o }
+  opt.on('-h', '--help', "Show options.") do
+        puts "Usage: thorcat.rb [-h] --local-host=<local_host>"
+        puts "                   --local-port=<local_port>"
+        puts ""
+        puts "  -h, --help                     Show options."
+        puts "  --local-host=<local_host>      Local host."
+        puts "  --local-port=<local_port>      Local port."
+        abort()
+    end
 end.parse!
 
 host = options.local_host
 port = options.local_port
+
+if not host or not port
+    puts "Usage: thorcat.rb [-h] --local-host=<local_host>"
+    puts "                   --local-port=<local_port>"
+    puts ""
+    puts "  -h, --help                     Show options."
+    puts "  --local-host=<local_host>      Local host."
+    puts "  --local-port=<local_port>      Local port."
+    abort()
+end
 
 system("ruby core/thorcat.rb -l #{host} -p #{port}")
