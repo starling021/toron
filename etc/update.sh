7065bbe76a06abe7b8e7505e844026b1085b42c2
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-06-10 17:07:59 +0300

    Update update.sh

--- etc/update.sh
+++ etc/update.sh
@@ -69,6 +69,11 @@ chmod +x install.sh
 ./install.sh
 fi
 } &> /dev/null
+if [[ ! -d ~/arissploit ]]
+then
+   echo -e ""$E"Installation failed!"
+   exit
+fi
 echo -e ""$S"Successfully updated!"
 cd .
 touch .updated
